e252ee4e7f4a9e4aee50513a99c24ea7d82589c7
Walter <Sphinxs@users.noreply.github.com>
2017-02-26 15:05:01 -0300

    Add files via upload

---
+++ Code002.cpp
@@ -0,0 +1,33 @@
+
+include <stdio.h>
+
+#include <string.h>
+
+void imprimeDecrescente(char *vet, int ini, int fim){
+
+     if(ini <= fim){
+
+        imprimeDecrescente(vet, ini+1, fim);
+
+        printf("%c", vet[ini]);
+
+     }
+
+     return;
+
+}
+
+int main () {
+
+    char vetor[100];
+
+    gets(vetor);
+
+    imprimeDecrescente(vetor, 0, strlen(vetor)-1);
+
+    printf("\n");
+
+    system("pause");
+
+    return 0;
+}
