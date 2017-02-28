06dea8073e884e7b83e04351bc79457b9b3ce710
Walter <Sphinxs@users.noreply.github.com>
2017-02-28 13:07:36 -0300

    Add files via upload

---
+++ Code006.cpp
@@ -0,0 +1,38 @@
+
+# include <stdio.h>
+
+# include <stdlib.h>
+
+# include <string.h>
+
+typedef struct Pessoa {
+
+    char marca[20], cor[15];
+
+    int portas, preco;
+
+}Peso;
+
+int main (void) {
+
+    Peso carro;
+
+    FILE *bin;
+    
+    bin = fopen ( "informacoes.bin", "rb" );
+
+    if ( bin == NULL ) {
+
+        exit(1);
+
+    }
+    
+        int gravado;
+
+        gravado = fread( &carro , sizeof(Peso), 1, bin);
+        
+                printf("%s %s %i %i",carro.marca,carro.marca,carro.portas,carro.preco);
+
+    fclose(bin);
+
+}
