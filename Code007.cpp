3b2a15cd930bdd1ab44d2a924c8701254661dfec
Walter <Sphinxs@users.noreply.github.com>
2017-03-12 10:00:33 -0300

    Update Code007.cpp

--- Code007.cpp
+++ Code007.cpp
@@ -27,9 +27,9 @@ int main ( int argc, char const *argv[] ) {
 
 	// Caso retorne ' -1 ' o valor retornado foi estourado ( Undexflow / Overflow )
 
-    if( resultado != -1) {
+    	if( resultado != -1) {
 
-        printf("\n\nHora da execução: %s\n", asctime(gmtime(&resultado)), (uintmax_t)resultado);
+        	printf("\n\nHora da execução: %s\n", asctime(gmtime(&resultado)), (uintmax_t)resultado);
 
 	}
 
