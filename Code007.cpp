407e651712ae734577434e066e5748ba12a10913
Walter <Sphinxs@users.noreply.github.com>
2017-03-18 10:19:43 -0300

    Update Code007.cpp

--- Code007.cpp
+++ Code007.cpp
@@ -1,6 +1,4 @@
 
-// Diretivas de pré processamento 
-
 # include <stdio.h>
 
 # include <time.h>
@@ -9,14 +7,6 @@
 
 # include <locale.h>
 
-/*
-	<time> De fato obtém o tempo
-	<locale> Indica o idioma utf-8
-	<stdio> Entrada e saída padrão
-	<stdint> Auxilia na formatação
-*/
-
-// Argumentos de linha de comando ( Argc, Argv )
 
 int main ( int argc, char const *argv[] ) {
 
@@ -27,9 +17,7 @@ int main ( int argc, char const *argv[] ) {
 	// Caso retorne ' -1 ' o valor retornado foi estourado ( Undexflow / Overflow )
 
     	if( resultado != -1) {
-
         	printf("\n\nHora da execução: %s\n", asctime(gmtime(&resultado)), (uintmax_t)resultado);
-
 	}
 
 	return 0;
