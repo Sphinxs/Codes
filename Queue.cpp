49aace24a3ec4beda9977ba528fb1e2e203f562c
Walter <Sphinxs@users.noreply.github.com>
2018-06-07 16:09:32 -0300

    C

---
+++ Queue.cpp
@@ -0,0 +1,143 @@
+
+#include <iostream>
+
+using namespace std;
+
+template <class tp>
+class Queue
+{
+  private:
+    int size;
+
+    tp *vector;
+
+    template <class t>
+    friend std::ostream &operator<<(std::ostream &, const Queue<t> &);
+
+  public:
+    Queue()
+    {
+        this->size = 0;
+
+        this->vector = new tp[this->size];
+    }
+
+    Queue(const int size)
+    {
+        this->size = size;
+
+        this->vector = new tp[this->size];
+    }
+
+    Queue(const Queue<tp> &obj)
+    {
+        if (this->size > 0)
+            delete[] vector;
+
+        this->size = obj.size;
+
+        this->vector = new tp[this->size];
+
+        for (int counter = 0; counter < this->size; counter++)
+            this->vector[counter] = obj.vector[counter];
+    }
+
+    ~Queue(void)
+    {
+        delete[] this->vector;
+    }
+
+    Queue<tp> &operator=(const Queue<tp> &obj)
+    {
+        if (&obj != this)
+        {
+            if (obj.size != this->size)
+            {
+                this->size = obj.size;
+
+                delete[] this->vector;
+
+                this->vector = new tp[this->size];
+            }
+
+            for (int counter = 0; counter < this->size; counter++)
+                this->vector[counter] = obj.vector[counter];
+        }
+    }
+
+    tp &operator[](int indice)
+    {
+        if (indice < 0 || indice > this->size)
+            exit(1);
+
+        return this->vector[indice];
+    }
+
+    int getSize(void)
+    {
+        return this->size;
+    }
+
+    void enqueue(const tp item)
+    {
+
+        tp *cache = new tp[this->size + 1];
+
+        for (int counter = 0; counter < this->size; counter++)
+            cache[counter] = this->vector[counter];
+
+        cache[this->size++] = item;
+
+        delete[] this->vector;
+
+        vector = cache;
+    }
+
+    tp dequeue(void)
+    {
+        tp value = this->vector[0];
+
+        tp *cache = new tp[this->size - 1];
+        
+        int a = 0, b = 1;
+
+        while(a < this->size)
+            cache[a++] = this->vector[b++];
+
+        this->size--;
+
+        delete[] this->vector;
+
+        this->vector = cache;
+
+        return value;
+    }
+
+    void show(void)
+    {
+        std::cout << "\nVector [";
+
+        for (int counter = 0; counter <= this->size - 1; counter++)
+        {
+            if (counter == this->size - 1)
+                std::cout << this->vector[counter];
+            else
+                std::cout << this->vector[counter] << ", ";
+        }
+
+        std::cout << "]\n\n";
+    }
+};
+
+template <class t>
+std::ostream &operator<<(std::ostream &output, const Queue<t> &obj)
+{
+    output << obj.vector[0];
+
+    return output;
+}
+
+int main()
+{
+    return 0;
+}
